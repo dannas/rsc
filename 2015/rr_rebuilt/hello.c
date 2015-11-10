d212efe2b0c67c2dd26e04ec4648b9a81eabe97c
Daniel Näslund <dannas@dannas.name>
2015-11-10 22:23:47 +0100

    First (unfinished) v1 of rr dissection.

---
+++ 2015/rr_rebuilt/hello.c
@@ -0,0 +1,31 @@
+// A small testprogram that only executes two syscalls; write and exit.
+// Not linked to any library.
+
+#include <sys/types.h>
+#include <sys/syscall.h>
+
+int my_write(int fd, const char* buf, size_t count)
+{
+   size_t ret;
+  __asm__("syscall\n\t"
+          : "=a"(ret)
+          : "a"(SYS_write), "D"(fd), "S"(buf), "d"(count));
+  return ret;
+}
+
+int my_exit(int status)
+{
+    int ret;
+    __asm__("syscall\n\t"
+	    : "=a"(ret)
+	    : "a"(SYS_exit));
+    return ret;
+}
+
+void _start()
+{
+    const char s[] = "Hello world\n";
+    my_write(1, s, sizeof(s)-1);
+    my_exit(0);
+}
+
---
+++ 2015/rr_rebuilt/util.h
@@ -0,0 +1,54 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <vector>
+#include <string>
+
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstdarg>
+#include <cerrno>
+#include <cstring>          // strerror
+
+#include <sys/personality.h>
+#include <sys/ptrace.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <sys/user.h>
+#include <sys/reg.h>        // Constants ORIG_RAX, RAX, ...
+#include <sys/wait.h>
+#include <unistd.h>
+
+/**
+ * Converts a vector of strings to a POSIX-style array of char*s terminated
+ * by a nullptr.
+ */
+class StringVectorToCharArray {
+public:
+  StringVectorToCharArray(const std::vector<std::string>& vs) {
+    for (auto& v : vs) {
+      array.push_back(const_cast<char*>(v.c_str()));
+    }
+    array.push_back(nullptr);
+  }
+  char** get() { return array.data(); }
+private:
+  std::vector<char*> array;
+};
+
+
+void die(const char *fmt, ...);
+
+pid_t xfork();
+
+long xptrace(enum __ptrace_request request, pid_t pid,
+             void *addr, void *data);
+
+void xexecvp(const char* file, char* const argv[]);
+
+void xkill(pid_t pid, int sig);
+
+void xpersonality(unsigned long persona);
+
+#endif
