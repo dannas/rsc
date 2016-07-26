a7469a8e8b9c0aeafbd9523f93ea9d0b46c4f1d5
Daniel Näslund <dannas@dannas.name>
2016-07-26 10:06:28 +0200

    vector_performance: Add benchmark results.

--- 2016/vector_performance/benchmark_append.cpp
+++ 2016/vector_performance/benchmark_append.cpp
@@ -3,76 +3,70 @@
 
 // Benchmark various ways of appending integers to a vector.
 //
-// Surprisingly to me, vector::push_back() is ~9x slower than
-// access using iterators or indexes. Using reserve() + push_back() was
-// as slow as just running push_back().
-//
-// We have 500x more page-faults for push_back() vs non-push_back().
-//
-// $ perf stat ./benchmark_append --benchmark-filter=BM_New | grep page-faults
-// 126 page-faults
-//
-// $ perf stat ./benchmark_append --benchmark-filter=BM_VectorPushBack | grep page-faults
-// 67,433 page-faults
-//
-// I guess it's because the OS does lazy loading of physical pages that have
-// not yet been accessed. Reserve requests pages, but do not touch them.
-//
 // $ ./benchmark_append 
-// Run on (8 X 840.023 MHz CPU s)
-// 2016-07-25 21:44:42
-// Benchmark                             Time           CPU Iterations
-// -------------------------------------------------------------------
-// BM_New/8                              3 ns          3 ns  243055556
-// BM_New/64                            25 ns         25 ns   27777778
-// BM_New/512                          154 ns        153 ns    4487179
-// BM_New/4k                          1200 ns       1200 ns     583333
-// BM_New/8k                          2400 ns       2400 ns     291667
-// BM_VectorResizeIndex/8                3 ns          3 ns  216049383
-// BM_VectorResizeIndex/64              25 ns         25 ns   28225806
-// BM_VectorResizeIndex/512            155 ns        154 ns    4487179
-// BM_VectorResizeIndex/4k            1201 ns       1200 ns     583333
-// BM_VectorResizeIndex/8k            2393 ns       2386 ns     291667
-// BM_VectorPushBack/8                  19 ns         19 ns   30701754
-// BM_VectorPushBack/64                188 ns        187 ns    4375000
-// BM_VectorPushBack/512              1486 ns       1486 ns     530303
-// BM_VectorPushBack/4k               9763 ns       9744 ns      60345
-// BM_VectorPushBack/8k              19448 ns      19412 ns      30702
-// BM_VectorReservePushback/8           19 ns         19 ns   30701754
-// BM_VectorReservePushback/64         186 ns        186 ns    4487179
-// BM_VectorReservePushback/512       1503 ns       1501 ns     530303
-// BM_VectorReservePushback/4k        9852 ns       9806 ns      58333
-// BM_VectorReservePushback/8k       19415 ns      19456 ns      31250
-// BM_VectorResizeIterator/8             4 ns          4 ns  190217391
-// BM_VectorResizeIterator/64           27 ns         27 ns   25735294
-// BM_VectorResizeIterator/512         159 ns        159 ns    4375000
-// BM_VectorResizeIterator/4k         1200 ns       1200 ns     583333
-// BM_VectorResizeIterator/8k         2395 ns       2384 ns     286885
+// Run on (8 X 3408.03 MHz CPU s)
+// 2016-07-26 09:54:02
+// Benchmark                              Time           CPU Iterations
+// --------------------------------------------------------------------
+// BM_New/8                              19 ns         26 ns   28225806
+// BM_New/64                             86 ns         63 ns   10937500
+// BM_New/512                           142 ns        187 ns    3723404
+// BM_New/4k                           1679 ns       1233 ns     564516
+// BM_New/32k                          7803 ns      10281 ns      67308
+// BM_New/256k                       117875 ns      89368 ns       7609
+
+// BM_VectorResizeIndex/8                19 ns         25 ns   27777778
+// BM_VectorResizeIndex/64              116 ns         85 ns    8333333
+// BM_VectorResizeIndex/512             257 ns        338 ns    2058824
+// BM_VectorResizeIndex/4k             1847 ns       2426 ns     286885
+// BM_VectorResizeIndex/32k           26938 ns      20516 ns      34314
+// BM_VectorResizeIndex/256k         135784 ns     177938 ns       3889
+
+// BM_VectorPushBack/8                  173 ns        130 ns    5833333
+// BM_VectorPushBack/64                 306 ns        401 ns    1785714
+// BM_VectorPushBack/512               3039 ns       2000 ns     324074
+// BM_VectorPushBack/4k               10115 ns      13351 ns      53030
+// BM_VectorPushBack/32k             136651 ns     102214 ns       6731
+// BM_VectorPushBack/256k            915414 ns    1199269 ns        547
+
+// BM_VectorReservePushback/8            34 ns         45 ns   15909091
+// BM_VectorReservePushback/64          318 ns        240 ns    2966102
+// BM_VectorReservePushback/512        1148 ns       1507 ns     448718
+// BM_VectorReservePushback/4k        16310 ns      12116 ns      56452
+// BM_VectorReservePushback/32k       74880 ns      98738 ns       7292
+// BM_VectorReservePushback/256k    1099588 ns     835534 ns        833
+
+// BM_VectorResizeIterator/8             22 ns         29 ns   23333333
+// BM_VectorResizeIterator/64           134 ns         99 ns    7608696
+// BM_VectorResizeIterator/512          259 ns        339 ns    1944444
+// BM_VectorResizeIterator/4k          1856 ns       2440 ns     286885
+// BM_VectorResizeIterator/32k        27029 ns      20516 ns      34314
+// BM_VectorResizeIterator/256k      135604 ns     178966 ns       3889
 
 using namespace std;
 
 static void BM_New(benchmark::State& state) {
     int N = state.range_x();
-    int* bigarray = new int[N];
-    benchmark::DoNotOptimize(bigarray);
 
     while (state.KeepRunning()) {
+        int* bigarray = new int[N];
+        benchmark::DoNotOptimize(bigarray);
 
         for (unsigned int k = 0; k<N; ++k) {
             bigarray[k] = k;
             benchmark::ClobberMemory();
         }
+        delete [] bigarray;
     }
-    delete [] bigarray;
 }
-BENCHMARK(BM_New)->Range(8, 8<<10);
+BENCHMARK(BM_New)->Range(8, 8<<15);
 
 static void BM_VectorResizeIndex(benchmark::State& state) {
     int N = state.range_x();
-    vector<int> bigarray(N);
-    benchmark::DoNotOptimize(bigarray.data());
 
     while (state.KeepRunning()) {
+        vector<int> bigarray(N);
+        benchmark::DoNotOptimize(bigarray.data());
 
         for (unsigned int k = 0; k<N; ++k) {
             bigarray[k] = k;
@@ -80,14 +74,14 @@ static void BM_VectorResizeIndex(benchmark::State& state) {
         }
     }
 }
-BENCHMARK(BM_VectorResizeIndex)->Range(8, 8<<10);
+BENCHMARK(BM_VectorResizeIndex)->Range(8, 8<<15);
 
 static void BM_VectorPushBack(benchmark::State& state) {
     int N = state.range_x();
-    vector<int> bigarray;
-    benchmark::DoNotOptimize(bigarray.data());
 
     while (state.KeepRunning()) {
+        vector<int> bigarray;
+        benchmark::DoNotOptimize(bigarray.data());
 
         for (unsigned int k = 0; k<N; ++k) {
             bigarray.push_back(k);
@@ -95,15 +89,15 @@ static void BM_VectorPushBack(benchmark::State& state) {
         }
     }
 }
-BENCHMARK(BM_VectorPushBack)->Range(8, 8<<10);
+BENCHMARK(BM_VectorPushBack)->Range(8, 8<<15);
 
 static void BM_VectorReservePushback(benchmark::State& state) {
     int N = state.range_x();
-    vector<int> bigarray;
-    bigarray.reserve(N);
-    benchmark::DoNotOptimize(bigarray.data());
 
     while (state.KeepRunning()) {
+        vector<int> bigarray;
+        bigarray.reserve(N);
+        benchmark::DoNotOptimize(bigarray.data());
 
         for (unsigned int k = 0; k<N; ++k) {
             bigarray.push_back(k);
@@ -111,15 +105,15 @@ static void BM_VectorReservePushback(benchmark::State& state) {
         }
     }
 }
-BENCHMARK(BM_VectorReservePushback)->Range(8, 8<<10);
+BENCHMARK(BM_VectorReservePushback)->Range(8, 8<<15);
 
 static void BM_VectorResizeIterator(benchmark::State& state) {
     int N = state.range_x();
-    vector<int> bigarray;
-    bigarray.resize(N);
-    benchmark::DoNotOptimize(bigarray.data());
 
     while (state.KeepRunning()) {
+        vector<int> bigarray;
+        bigarray.resize(N);
+        benchmark::DoNotOptimize(bigarray.data());
 
         auto i = begin(bigarray);
         for(unsigned int k = 0; k<N; ++k) {
@@ -128,7 +122,7 @@ static void BM_VectorResizeIterator(benchmark::State& state) {
         }
     }
 }
-BENCHMARK(BM_VectorResizeIterator)->Range(8, 8<<10);
+BENCHMARK(BM_VectorResizeIterator)->Range(8, 8<<15);
 
 
 BENCHMARK_MAIN();
