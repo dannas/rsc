ffd20038a796d6d6b07a4f2ccc98447163c1705b
Daniel Näslund <dannas@dannas.name>
2016-09-26 21:28:17 +0200

    vm: Add exec integration test.

---
+++ 2016/vm/exec_integrationtest.cpp
@@ -0,0 +1,39 @@
+
+#include "vm.h"
+
+#include <cstdint>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+using namespace std;
+
+#define EXEC_AND_COMPARE(buf, expected) \
+    do { \
+        stringstream as(buf); \
+        auto code = assemble(as); \
+        int32_t globals[] = {}; \
+        stringstream ss; \
+        interpret(code.data(), globals, ss); \
+        ss.seekg(0); \
+        string actual = ss.str(); \
+        ASSERT_EQ(actual, expected); \
+    } while (0)
+
+TEST(Integration, simplePrint) {
+    char buf[] = R"(
+        iconst 1
+        iconst 2
+        iadd
+        print
+        halt
+    )";
+    string expected = "3\n";
+
+    EXEC_AND_COMPARE(buf, expected);
+}
+
+int main(int argc, char* argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
