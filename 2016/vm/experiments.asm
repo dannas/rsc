8f70078afb9ac4604018a44f88e4f237ccb24f8d
Daniel Näslund <daniel@titv.se>
2021-08-10 19:44:58 +0200

    vm: Add asm file

---
+++ 2016/vm/experiments.asm
@@ -0,0 +1,10 @@
+
+; prefix opcode modR/M SIB displacement immediate
+
+foo:
+    mov [rax+0x18], rax
+    mov [rcx+0x18], rax
+    mov [rdx+0x18], rax
+    mov [rbx+0x18], rax
+    mov [rsp+0x18], rax
+
